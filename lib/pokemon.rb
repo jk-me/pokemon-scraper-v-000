f63bafaf86b2918f93924a112aa95cbf87817e48
Jenny K <jkam17@gmail.com>
2018-09-16 03:09:33 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,14 +9,15 @@ class Pokemon
     @hp = hp
   end
 
-  def self.save (name, type, db)
-    db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)", name,type)
+  def self.save (name, type, hp, db)
+    db.execute("INSERT INTO pokemon (name,type) VALUES (?,?,?)", name,type,hp)
   end
 
   def self.find(index, db)
     a = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", index)
     b = a[0]
-    Pokemon.new(id:b[0],name:b[1],type:b[2], db:db, hp: 60)
-
+    x = Pokemon.new(id:b[0],name:b[1],type:b[2], db:db, hp: 60)
+    Pokemon.save(x.name,x.type,x.hp)
+    x
   end
 end
